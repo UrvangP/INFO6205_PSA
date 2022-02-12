a0fbb1bbc519f32125df66e09e9847b1412e5c81
Urvang <patel.ur@northeastern.edu>
2022-02-12 13:12:24 -0500

    First Commit

---
+++ src/main/java/edu/neu/coe/info6205/randomwalk/randomWalkRelation.java
@@ -0,0 +1,18 @@
+package edu.neu.coe.info6205.randomwalk;
+
+public class randomWalkRelation {
+
+    public static void main(String[] args){
+        RandomWalk.randomWalkMulti(1,10);
+        RandomWalk.randomWalkMulti(2,10);
+        RandomWalk.randomWalkMulti(3,10);
+        RandomWalk.randomWalkMulti(4,10);
+        RandomWalk.randomWalkMulti(5,10);
+        RandomWalk.randomWalkMulti(10,50);
+        RandomWalk.randomWalkMulti(20,50);
+        RandomWalk.randomWalkMulti(50,50);
+        RandomWalk.randomWalkMulti(100,100);
+        RandomWalk.randomWalkMulti(500,50);
+        RandomWalk.randomWalkMulti(1000,100);
+    }
+}
